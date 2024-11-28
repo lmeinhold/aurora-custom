2a4cabbf52d7299b35d09fa0e418cb5e0f2b097e
Lukas Meinhold <lukas@meinhold.dev>
2024-11-28 02:23:09 +0100

    Add btrfs-assistant and some fonts

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install nextcloud-client ripgrep java-21-openjdk tlp ansible emacs-nw micro neovim
+rpm-ostree install nextcloud-client ripgrep java-21-openjdk tlp ansible emacs-nw micro neovim btrfs-assistant ibm-plex-fonts-all rsms-inter-fonts google-roboto-fonts
 
 #### Example for enabling a System Unit File
 # systemctl enable tlp.service
